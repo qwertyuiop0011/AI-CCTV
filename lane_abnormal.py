8bf38040948101b0e669622ea19414d50a495a23
ckim48 <madckkim@gmail.com>
2023-04-17 08:38:40 +0900

    Add files via upload

--- lane_abnormal.py
+++ lane_abnormal.py
@@ -108,13 +108,43 @@ def trackMultipleObjects():
 
     while frameCounter<5396:
         rc, image = video.read()
+        if frameCounter == 0:
+            left_avg=np.array([])
+            right_avg=np.array([])
+            #img = getFirstFrame('test/accident_3.mp4')
+            lst =[]
+            gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+            kernel_size = 5
+            blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+            low_threshold = 50
+            high_threshold = 150
+            edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+            rho = 1  
+            theta = np.pi / 180  
+            threshold = 100  
+            min_line_length = 10  
+            max_line_gap = 2000  
+            line_image = np.copy(img) * 0  
+
+            lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                                    min_line_length, max_line_gap)
+
+            for line in lines:
+                for x1,y1,x2,y2 in line:
+                    cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+                    lst.append([(x1,y1),(x2,y2)])
+            
         if rc == False:
             break
         if type(image) == type(None):
             break
         
         resultImage = image.copy()
-        
+        resultImage = cv2.addWeighted(image, 0.8, line_image, 1, 0)
+
         frameCounter = frameCounter + 1
         
         carIDdelete = []
